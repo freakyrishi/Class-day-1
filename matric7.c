f2a8d66290b86950b70b98197fcb10652aa6c623
freakyrishi <95204461+freakyrishi@users.noreply.github.com>
2021-12-09 08:04:40 +0530

    Add files via upload

---
+++ matric7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int r,c;
+	for (r='a'; r<='d'; r++)
+	{
+		for (c='a'; c<=r; c++)
+		{
+			printf("%c",c);
+		}
+			printf("\n");
+	}
+}
---
+++ matrix5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	char r,c;
+	for (r=1; r<=4; r++)
+	{
+		for (c=1; c<=5; c++)
+		{
+			printf("a");
+		}
+			printf("\n");
+	}
+}
